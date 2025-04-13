38b5c75d05a236b2f21b77cc61ffb3d91ff86e1b
Vimala <vimalaKS@gmail.com>
2025-04-13 08:58:59 +0530

    New Project for GROQ EXpress server added in git

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const {GROQ} = require('groq-sdk');
+const client = new Groq({
+    apiKey: process.env['GROQ_API_KEY'], // This is the default and can be omitted
+  });
+const dotenv = require('dotenv');
+const { APIClient } = require('groq-sdk/core');
+dotenv.config();
+const PORT = 8000;
+
+app.get('/test',(req,res,next)=>
+{
+    console.log("Get api is accessed");
+    res.status(200).json({msg:'GET api is successfully accessed'});
+});
+app.post('/prompt',(req,res,next)=>
+{
+    const chatCompletion = await client.chat.completions.create({
+        messages: [{ role: 'user', content: 'Explain the importance of low latency LLMs' }],
+        model: 'llama3-8b-8192',
+      });
+      res.status(200).json({response:chatCompletion['choices[0].message.content']});
+});
+app.listen(PORT,()=>
+{
+    console.log(`Server is up and running and listening @ ${PORT}`);
+    console.log(process.env['GROQ_API_KEY']);
+});

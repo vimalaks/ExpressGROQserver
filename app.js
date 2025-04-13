e12b4682c52e30b326243eddffe140434caa790a
Vimala <vimalaKS@gmail.com>
2025-04-13 09:56:48 +0530

    replacing "require with import" to mitigate "Groq not defined" error

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const GROQ = require('groq-sdk');
+//const GROQ = require('groq-sdk');
+import Groq from 'groq-sdk';
 const client = new Groq({
     apiKey: process.env['GROQ_API_KEY'], // This is the default and can be omitted
   });
